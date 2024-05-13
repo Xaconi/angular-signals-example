5f29a46ceb467c70c48813fa8ef140e0bdb83575
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2024-05-13 19:52:30 +0200

    Add header home link

--- src/app/core/ase-header/ase-header.component.ts
+++ src/app/core/ase-header/ase-header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { AseCartComponent } from '../ase-cart/ase-cart.component';
 
 @Component({
   selector: 'ase-header',
   standalone: true,
-  imports: [AseCartComponent],
+  imports: [AseCartComponent, RouterModule],
   templateUrl: './ase-header.component.html',
   styleUrl: './ase-header.component.scss'
 })
